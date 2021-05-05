7821fb49722f63db52baea361d3120341e313686
yu1hpa <yu.1hpa@gmail.com>
2021-05-05 21:47:52 +0900

    feat(b): add source code

---
+++ ABC/ABC088/B.rs
@@ -0,0 +1,19 @@
+use proconio::input;
+fn main(){
+  input! {
+    n: usize,
+    mut a: [i32; n],
+  }
+  a.sort();
+  a.reverse();
+
+  let mut ans=0;
+  for i in 0..n {
+    if i%2 == 0 {
+      ans += a[i];
+    } else {
+      ans -= a[i];
+    }
+  }
+  println!("{}", ans);
+}
